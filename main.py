7ba4494f23d192a37bf0b9b6171e8072e0118541
Luke <luke.floden@gmail.com>
2021-04-11 18:54:28 -0400

    All experiments are running and providing required results

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass, field
 from random import shuffle, randint
-from typing import Any, List, Tuple
+from typing import Any, List, Optional, Tuple
 
 import argh
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from pandas.core.indexes.base import Index
 import seaborn as sns
 from numpy import float64, ndarray
 from pandas.core.frame import DataFrame
@@ -44,23 +45,19 @@ class Student():
             other.hear()
 
 
-def main(student_cnt: int = 2, stop_time: int = 100, display: bool = False):
+def main(student_cnt: int = 2, stop_time: int = 2000, stop_percent: Optional[float] = None, display: bool = False):
     assert student_cnt >= 2 and student_cnt <= 10000
     students = [Student() for _ in range(student_cnt)]
     students[0].hear()
 
-    def amount_heard():
-        return sum([s.heard() for s in students])
-
-    time = 0
-    # Simulation Loop
-    # stop_time = 2
     if display:
         console.rule(f'cnt={student_cnt}, stop={stop_time}')
-    while time < stop_time:
-        if display:
-            p(amount_heard())
 
+    def percent_heard():
+        return sum([s.heard() for s in students]) / student_cnt
+
+    time = 0
+    while time < stop_time:
         shuffle(students)
         split = student_cnt//2
         firsts = students[:split]
@@ -70,62 +67,112 @@ def main(student_cnt: int = 2, stop_time: int = 100, display: bool = False):
             first = firsts[i]
             second = seconds[i]
 
-            # if first.able_to_tell():
-            #     first.tell(second)
-            # elif second.able_to_tell():
-            #     second.tell(first)
-
             if first.able_to_tell() and second.able_to_tell():
-                first.hear()
-                second.hear()
+                if randint(0, 1):
+                    first.hear()
+                if randint(0, 1):
+                    second.hear()
             elif first.able_to_tell():
                 first.tell(second)
             elif second.able_to_tell():
                 second.tell(first)
 
         time += 1
+        if stop_percent is not None:
+            if percent_heard() >= stop_percent:
+                break
 
-    # Every person who has knowledge of the rumor obeys these rules:
-    # 1. The likelihood of spreading a rumor to another person is 0.5
-    # 2. After a person has heard the rumor 2 times, he/she will assume
-    # everyone has heard the rumor and will no longer try to spread it further.
+        if display:
+            p(time, percent_heard)
 
-    # return (amount_heard / student_cnt) * 100
-    return (amount_heard() / student_cnt)
+    return percent_heard(), time
 
 
-def avg_main(student_cnt: int = 2, stop_time: int = 100, trials: int = 10):
-    tot = 0
-    for t in range(trials):
-        tot += main(student_cnt, stop_time)
+def avg_main_time(student_cnt: int = 2, stop_time: int = 100, trials: int = 10):
+    tot = 0.0
+    for t in track(range(trials), description=f'Trials (students={student_cnt}, max_time={stop_time}) '):
+        tot += main(student_cnt, stop_time)[0]
     tot /= trials
     return tot
 
 
-def experiment():
-    inds = [100, 1000, 10000]
-    # data = [[main(n, 10), main(n, 20), main(n, 40)] for n in inds]
-    data = [[avg_main(n, 10), avg_main(n, 20), avg_main(n, 40)] for n in inds]
-    cols = [10, 20, 40]
-    df = pd.DataFrame(data, columns=cols, index=inds)
-    p(df)
-    with console.status('Plotting'):
-        sns.set()
-        ax = sns.heatmap(df, annot=True)
-        ax.invert_yaxis()
-        plt.title('Rumor Spread vs. Time')
-        plt.xlabel('Maximum Minutes')
-        plt.ylabel('Student Count')
-        plt.tight_layout()  # type: ignore
-        plt.show()
+def avg_main_percent(student_cnt: int = 2, stop_percent: float = 1.0, trials: int = 10):
+    tot = 0.0
+    for t in track(range(trials), description=f'Trials (students={student_cnt}, stop_percent={stop_percent}) '):
+        tot += main(student_cnt, stop_percent=stop_percent)[1]
+    tot /= trials
+    return tot
 
 
-# run your simulation several times to determine:
-# 1. On average, what % of the attendees will have heard the rumor after 10 minutes?
-# 2. On average, what % of the attendees will have heard the rumor after 20 minutes?
-# 3. On average, what % of the attendees will have heard the rumor after 40 minutes?
-# 4. At what time, t, will 10 % of the party have heard the rumor? N = 10, 000.
-# 5. At what time, t, will 50 % of the party have heard the rumor? N = 10, 000.
+def experiment(trials: int = 1000, run_max_time: bool = False, run_max_percent: bool = True):
+    if run_max_time:
+        console.rule('[bold blue] Maximum Time')
+        inds = [100, 1000, 10000]
+        cols = [10, 20, 40]
+        data = [[avg_main_time(n, 10, trials), avg_main_time(n, 20, trials),
+                 avg_main_time(n, 40, trials)] for n in inds]
+        df = pd.DataFrame(data, columns=cols, index=inds)
+
+        with console.status('Plotting'):
+            def percent(x):
+                return f'{(x*100):0.2f}%'
+            annots = [[percent(col) for col in row] for row in data]
+
+            sns.set()
+            ax = sns.heatmap(df, annot=annots, fmt='')
+            ax.invert_yaxis()
+            plt.title(f'Rumor Spread vs. Time (Avgeraged over {trials} trials)')
+            plt.xlabel('Maximum Minutes')
+            plt.ylabel('Student Count')
+            plt.tight_layout()  # type: ignore
+            plt.show()
+
+            sns.lineplot(data=df.T)
+            plt.title(f'Rumor Spread over time for different Student Counts \n(Avgeraged over {trials} trials)')
+            plt.xlabel('Minutes')
+            plt.legend(title='Student Count')   # type: ignore
+            plt.ylabel('Rumor Spread %')
+            plt.tight_layout()  # type: ignore
+            plt.show()
+
+    if run_max_percent:
+        console.rule('[bold blue] Maximum Percent')
+        # inds = [100, 1000, 10000]
+        inds = [10000]
+        cols = ['10%', '50%']
+        avg_main_percent
+        data = [[avg_main_percent(n, .10, trials), avg_main_percent(n, .50, trials)] for n in inds]
+        df = pd.DataFrame(data, columns=cols, index=inds)
+
+        with console.status('Plotting'):
+            sns.set()
+
+            annots = [[str(col) for col in row] for row in data]
+            # ax = sns.heatmap(df, annot=annots, fmt='')
+            ax = sns.heatmap(df, annot=True)
+            ax.invert_yaxis()
+
+            plt.title(f'Minutes till Percentage Reached\n(Avgeraged over {trials} trials)')
+            plt.xlabel('Target Percentage')
+            plt.ylabel('Student Count')
+            plt.tight_layout()  # type: ignore
+            plt.show()
+
+            sns.lineplot(data=df.T)
+            plt.title(f'Rumor Spread over time for different Student Counts \n(Avgeraged over {trials} trials)')
+            plt.xlabel('Target Percentage')
+            plt.legend(title='Student count')   # type: ignore
+            plt.ylabel('Minutes taken')
+            plt.tight_layout()  # type: ignore
+            plt.show()
+
+    # run your simulation several times to determine:
+    # 1. On average, what % of the attendees will have heard the rumor after 10 minutes?
+    # 2. On average, what % of the attendees will have heard the rumor after 20 minutes?
+    # 3. On average, what % of the attendees will have heard the rumor after 40 minutes?
+    # 4. At what time, t, will 10 % of the party have heard the rumor? N = 10, 000.
+    # 5. At what time, t, will 50 % of the party have heard the rumor? N = 10, 000.
+
 
 if __name__ == '__main__':
     argh.dispatch_commands([main, experiment])
